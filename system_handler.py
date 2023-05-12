a4786939de6660eaf1eeef0112ff11c77949057a
gk <gk@MBA23A.local>
2023-05-12 12:33:09 +0800

    trying out some safety features

--- system_handler.py
+++ system_handler.py
@@ -3,6 +3,7 @@ import re
 import socket
 
 
+# convert to the single hashrate
 def get_hashrates():
     # Implement a function to fetch GPU hashrates using the provided command
     result = subprocess.run(
@@ -20,6 +21,24 @@ def get_hashrates():
 
     return hashrates
 
+
+# a single hashrate harvest
+def get_hashrate():
+    result = subprocess.run(
+        ["systemctl", "status", "abel.service"],
+        capture_output=True,
+        text = True,
+    )
+    hashrate_line = re.search(r"(\d+(?:\.\d+)?)[ ]?([mMkK]?)[hH].*- cu", result.stdout)
+    if hashrate_line:
+        hashrate_str = hashrate_line.group()
+        hashrate_tuple = re.findall(r"(\d+(?:\.\d+)?)[ ]?([mMkK]?)[hH]"), hashrate_str)
+        value, unit, _ = hashrate_tuple if len(hashrate_tuple) > 1 else []
+        hashrate = convert_to_mh(value, unit)
+    else:
+        0
+    
+    return hashrate
 
 def convert_to_mh(value, unit):
     try:
@@ -46,6 +65,12 @@ def get_time_since_last_boot():
         uptime_seconds = float(f.readline().split()[0])
 
     return uptime_seconds
+
+
+def get_system_data():
+    hostname = get_hostname()
+    hashrate = get_hashrates()
+    reboot_seconds = get_time_since_last_boot()
 
 
 if __name__ == "__main__":
